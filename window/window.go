5e44c9f86b269ad3a547017cf987031e70690ddd
Ryan Williams <git@ryan.nz>
2020-02-23 21:17:26 +1300

    Window catches ctrl+c

--- window/window.go
+++ window/window.go
@@ -66,6 +66,19 @@ func (w *Window) Open() {
 	w.Terminal.Clear()
 	w.Terminal.Flush()
 	w.ListenForResize()
+
+	// Catch Ctrl+C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			switch sig {
+			case os.Interrupt:
+				w.Close()
+				os.Exit(0)
+			}
+		}
+	}()
 }
 
 func (w *Window) Close() {
